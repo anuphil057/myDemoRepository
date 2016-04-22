1b8def13bb82d53682cf781aafaed16d6ccb5c99
anuphil057 <anuphil057@gmail.com>
2016-04-22 12:30:07 -0500

    Initial Commit

---
+++ myTestApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  myTestApp
+//
+//  Created by Anu Philip on 4/22/16.
+//  Copyright © 2016 RJT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ myTestApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  myTestApp
+//
+//  Created by Anu Philip on 4/22/16.
+//  Copyright © 2016 RJT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
